ca3e6d714698964d161fd2e61d751504a60bc541
Mandrake <49409357+mandrakean@users.noreply.github.com>
2024-01-06 03:13:46 -0400

    Add files via upload

---
+++ Javascript_Guan/ex013.js
@@ -0,0 +1,26 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+switch(diaSem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Domingo')
+        break
+    case 2:
+        console.log('Segunda')
+        break
+    case 3:
+    console.log('Terça')
+        break
+    case 4:
+    console.log('Quarta')
+        break
+    case 5:
+    console.log('Quinta')
+        break
+    case 6:
+    console.log('Sexta')
+        break
+}
